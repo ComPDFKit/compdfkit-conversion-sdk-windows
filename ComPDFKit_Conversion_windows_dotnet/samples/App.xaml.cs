d2192686e94e96e4ddac9f6c6e69bf07e3ce6c33
yanxin777 <149142254+yanxin777@users.noreply.github.com>
2025-06-09 16:46:28 +0800

    V3.0.0 - Update

---
+++ ComPDFKit_Conversion_windows_dotnet/samples/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace ComPDFKit_Conversion_Demo
+{
+    public partial class App : Application
+    {
+        
+    }
+}
---
+++ ComPDFKit_Conversion_windows_dotnet/samples/ConverterOptionsWindow.xaml.cs
@@ -0,0 +1,264 @@
+﻿using ComPDFKit_Conversion.Common;
+using System.Windows;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ComPDFKit_Conversion_Demo
+{
+  public partial class ConverterOptionsWindow : Window
+  {
+    private MainWindow parent;
+
+    public ConverterOptionsWindow(MainWindow parent, string type)
+    {
+      InitializeComponent();
+      this.parent = parent;
+
+      ContainAnnotations.Click += (sender, args) =>
+      {
+        if (ContainAnnotations.IsChecked != null)
+          this.parent.Options.ContainAnnotation = ContainAnnotations.IsChecked.Value;
+      };
+
+      CsvFormat.Click += (sender, args) =>
+      {
+        if (CsvFormat.IsChecked != null) this.parent.Options.CsvFormat = CsvFormat.IsChecked.Value;
+      };
+
+      AllContent.Click += (sender, args) =>
+      {
+        if (AllContent.IsChecked != null) this.parent.Options.AllContent = AllContent.IsChecked.Value;
+      };
+
+      ContainImages.Click += (sender, args) =>
+      {
+        if (ContainImages.IsChecked != null) this.parent.Options.ContainImage = ContainImages.IsChecked.Value;
+      };
+
+      EnableAiLayout.Click += (sender, args) =>
+      {
+        if (EnableAiLayout.IsChecked != null) this.parent.Options.EnableAiLayout = EnableAiLayout.IsChecked.Value;
+      };
+
+      EnableOCR.Click += (sender, args) =>
+      {
+        if (EnableOCR.IsChecked != null) this.parent.Options.EnableOCR = EnableOCR.IsChecked.Value;
+        OCRLanguage.IsEnabled = (bool)EnableOCR.IsChecked;
+      };
+
+      TxtTableFormat.Click += (sender, args) =>
+      {
+        if (TxtTableFormat.IsChecked != null) this.parent.Options.TxtTableFormat = TxtTableFormat.IsChecked.Value;
+      };
+
+      FormulaToImage.Click += (sender, args) =>
+      {
+        if (FormulaToImage.IsChecked != null) this.parent.Options.FormulaToImage = FormulaToImage.IsChecked.Value;
+      };
+
+      ImagePathEnhance.Click += (sender, args) =>
+      {
+        if (ImagePathEnhance.IsChecked != null) this.parent.Options.ImagePathEnhance = ImagePathEnhance.IsChecked.Value;
+      };
+
+      ContainTables.Click += (sender, args) =>
+      {
+        if (ContainTables.IsChecked != null) this.parent.Options.ContainTables = ContainTables.IsChecked.Value;
+      };
+
+      ImageFormat.SelectionChanged += ImageFormatComboBox_SelectionChanged;
+      ImageMode.SelectionChanged += ImageModeComboBox_SelectionChanged;
+
+      ExcelWorksheetOptions.SelectionChanged += ExcelWorksheetOptions_SelectionChanged;
+      HtmlOptions.SelectionChanged += HtmlOptions_SelectionChanged;
+
+      ImageRatio.LostFocus += ImageDpi_LostFocus;
+
+      OCRLanguage.SelectionChanged += OCRLanguage_SelectionChanged;
+
+      PageLayoutOptions.SelectionChanged += PageLayoutOptions_SelectionChanged;
+
+      EnableOCR.Checked += EnableOCR_Checked;
+      EnableOCR.Unchecked += EnableOCR_Unchecked;
+
+      switch (type)
+      {
+        case "Word":
+          IsContainImagesPanel.Visibility = Visibility.Visible;
+          IsContainAnnotationsPanel.Visibility = Visibility.Visible;
+          IsFormulaToImagePanel.Visibility = Visibility.Visible;
+          OCRLanguagePanel.Visibility = Visibility.Visible;
+          IsEnableOCRPanel.Visibility = Visibility.Visible;
+          IsEnableAiLayoutPanel.Visibility = Visibility.Visible;
+          PageLayoutOptionsPanel.Visibility = Visibility.Visible;
+          break;
+        case "Html":
+          IsContainImagesPanel.Visibility = Visibility.Visible;
+          IsContainAnnotationsPanel.Visibility = Visibility.Visible;
+          IsFormulaToImagePanel.Visibility = Visibility.Visible;
+          OCRLanguagePanel.Visibility = Visibility.Visible;
+          IsEnableOCRPanel.Visibility = Visibility.Visible;
+          IsEnableAiLayoutPanel.Visibility = Visibility.Visible;
+          PageLayoutOptionsPanel.Visibility = Visibility.Visible;
+          HtmlOptionsPanel.Visibility = Visibility.Visible;
+          break;
+        case "SearchablePDF":
+          IsContainImagesPanel.Visibility = Visibility.Visible;
+          IsFormulaToImagePanel.Visibility = Visibility.Visible;
+          OCRLanguagePanel.Visibility = Visibility.Visible;
+          OCRLanguage.IsEnabled = true;
+          break;
+        case "Ppt":
+        case "Rtf":
+          IsContainImagesPanel.Visibility = Visibility.Visible;
+          IsContainAnnotationsPanel.Visibility = Visibility.Visible;
+          IsFormulaToImagePanel.Visibility = Visibility.Visible;
+          OCRLanguagePanel.Visibility = Visibility.Visible;
+          IsEnableOCRPanel.Visibility = Visibility.Visible;
+          IsEnableAiLayoutPanel.Visibility = Visibility.Visible;
+          Height = 400;
+          break;
+        case "Markdown":
+          IsContainImagesPanel.Visibility = Visibility.Visible;
+          IsContainAnnotationsPanel.Visibility = Visibility.Visible;
+          OCRLanguagePanel.Visibility = Visibility.Visible;
+          IsEnableOCRPanel.Visibility = Visibility.Visible;
+          IsEnableAiLayoutPanel.Visibility = Visibility.Visible;
+          Height = 400;
+          break;
+        case "Excel":
+          IsContainImagesPanel.Visibility = Visibility.Visible;
+          IsContainAnnotationsPanel.Visibility = Visibility.Visible;
+          IsFormulaToImagePanel.Visibility = Visibility.Visible;
+          IsCsvFormatPanel.Visibility = Visibility.Visible;
+          IsAllContentPanel.Visibility = Visibility.Visible;
+          ExcelWorksheetOptionsPanel.Visibility = Visibility.Visible;
+          OCRLanguagePanel.Visibility = Visibility.Visible;
+          IsEnableOCRPanel.Visibility = Visibility.Visible;
+          IsEnableAiLayoutPanel.Visibility = Visibility.Visible;
+          Height = 400;
+          break;
+        case "Txt":
+          OCRLanguagePanel.Visibility = Visibility.Visible;
+          IsEnableOCRPanel.Visibility = Visibility.Visible;
+          IsEnableAiLayoutPanel.Visibility = Visibility.Visible;
+          IsTxtTableFormatPanel.Visibility = Visibility.Visible;
+          Height = 400;
+          break;
+        case "Image":
+          ImageRatioPanel.Visibility = Visibility.Visible;
+          ImageFormatPanel.Visibility = Visibility.Visible;
+          ImageModePanel.Visibility = Visibility.Visible;
+          IsImagePathEnhancePanel.Visibility = Visibility.Visible;
+          Height = 400;
+          break;
+        case "Json":
+          OCRLanguagePanel.Visibility = Visibility.Visible;
+          IsEnableOCRPanel.Visibility = Visibility.Visible;
+          IsContainImagesPanel.Visibility = Visibility.Visible;
+          IsContainAnnotationsPanel.Visibility = Visibility.Visible;
+          IsEnableAiLayoutPanel.Visibility = Visibility.Visible;
+          IsContainTablesPanel.Visibility = Visibility.Visible;
+          Height = 400;
+          break;
+      }
+    }
+
+    private void HtmlOptions_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    {
+      parent.Options.htmlOption = (HtmlPageOption)HtmlOptions.SelectedIndex;
+    }
+
+    private void ExcelWorksheetOptions_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    {
+      parent.Options.WorksheetOption = (ExcelWorksheetOption)ExcelWorksheetOptions.SelectedIndex;
+    }
+
+    private void PageLayoutOptions_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    {
+      parent.Options.LayoutMode = (PageLayoutMode)PageLayoutOptions.SelectedIndex;
+    }
+
+    private void CsvFormat_Click(object sender, RoutedEventArgs e)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      OCRLanguage.SelectedIndex = (int)parent.Options.OCRLanguage - 1;
+      ContainAnnotations.IsChecked = parent.Options.ContainAnnotation;
+      ContainImages.IsChecked = parent.Options.ContainImage;
+      EnableAiLayout.IsChecked = parent.Options.EnableAiLayout;
+      EnableOCR.IsChecked = parent.Options.EnableOCR;
+      CsvFormat.IsChecked = parent.Options.CsvFormat;
+      AllContent.IsChecked = parent.Options.AllContent;
+      TxtTableFormat.IsChecked = parent.Options.TxtTableFormat;
+      ImagePathEnhance.IsChecked = parent.Options.ImagePathEnhance;
+      ContainTables.IsChecked = parent.Options.ContainTables;
+      FormulaToImage.IsChecked = parent.Options.FormulaToImage;
+
+      ImageFormat.SelectedIndex = (int)parent.Options.ImageFormat;
+      ImageMode.SelectedIndex = (int)parent.Options.ImageMode;
+      ExcelWorksheetOptions.SelectedIndex = (int)parent.Options.WorksheetOption;
+      HtmlOptions.SelectedIndex = (int)parent.Options.htmlOption;
+
+      ImageRatio.Text = parent.Options.ImageRatio.ToString();
+
+      PageLayoutOptions.SelectedIndex = (int)parent.Options.LayoutMode;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      this.Close();
+    }
+
+    private void ImageFormatComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    {
+      parent.Options.ImageFormat = (ImageType)ImageFormat.SelectedIndex;
+    }
+
+    private void ImageModeComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    {
+      parent.Options.ImageMode = (ImageColorMode)ImageMode.SelectedIndex;
+    }
+
+    private void ImageDpi_LostFocus(object sender, RoutedEventArgs e)
+    {
+      float dpi;
+      if (float.TryParse(ImageRatio.Text, out dpi) && dpi > 0)
+      {
+        if (dpi > 100)
+        {
+          ImageRatio.Text = "100.0";
+          parent.Options.ImageRatio = 100.0f;
+        }
+        parent.Options.ImageRatio = dpi;
+      }
+      else
+      {
+        ImageRatio.Text = "1.0";
+        parent.Options.ImageRatio = 1.0f;
+      }
+    }
+
+    private void OCRLanguage_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    {
+      parent.Options.OCRLanguage = (OCRLanguage)OCRLanguage.SelectedIndex + 1;
+    }
+
+    private void EnableOCR_Checked(object sender, RoutedEventArgs e)
+    {
+      parent.Options.EnableOCR = (bool)EnableOCR.IsChecked;
+      OCRLanguage.IsEnabled = (bool)EnableOCR.IsChecked;
+      EnableAiLayout.IsEnabled = !parent.Options.EnableOCR;
+    }
+
+    private void EnableOCR_Unchecked(object sender, RoutedEventArgs e)
+    {
+      parent.Options.EnableOCR = (bool)EnableOCR.IsChecked;
+      OCRLanguage.IsEnabled = (bool)EnableOCR.IsChecked;
+      EnableAiLayout.IsEnabled = !parent.Options.EnableOCR;
+    }
+  }
+}
